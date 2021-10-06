7f697b2da60d3a77d92f4598003ae62384732bf8
moraru vlad <moraruvlad51@yahoo.com>
2021-10-06 18:57:38 +0300

    commit

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def get_temp(temp , fr , to):
 
     return temp
 
+
 def test_get_temp():
     '''
     Verifica daca functia get_temp() returneaza date corecte.
@@ -28,6 +29,7 @@ def test_get_temp():
     assert get_temp(273.0,'K','C') == 0.0
     assert get_temp(394.0,'F','C') == 201.11111111111111
     assert get_temp(123.0,'C','F') == 253.4
+
 
 def get_cmmmc(numbers):
     '''
@@ -48,6 +50,7 @@ def get_cmmmc(numbers):
 
     return int(cmmmc)
 
+
 def test_get_cmmc():
     '''
         Verifica daca functia get_cmmmc() returneaza date corecte.
@@ -56,10 +59,47 @@ def test_get_cmmc():
     assert get_cmmmc([20,12,59]) == 3540
     assert get_cmmmc([11,5,38,15]) == 6270
 
+
+def isPrime(x):
+    '''
+    Veridica daca un numar este prim
+    :param x: numarul dat, int
+    :return: True, daca numarul este prim, False, daca numarul nu este prim.
+    '''
+    if x<2:
+        return False
+    if x==2:
+        return True
+    for i in range(2,x//2+1):
+        if x%i==0:
+            return False
+    return True
+
+
+def get_largest_prime_below(n):
+    '''
+    Determina ultimul numar prim mai mic decat un numar dat.
+    :param n: numarul dat, int
+    :return:  ultimul numar prim mai mic decat un numar dat, int
+    '''
+    for i in range(n-1,2,-1):
+        if isPrime(i):
+            return i
+    return 0
+
+
+def test_get_largest_prime_below():
+    assert get_largest_prime_below(6)==5
+    assert get_largest_prime_below(5)==3
+    assert get_largest_prime_below(32)==29
+    assert get_largest_prime_below(108)==107
+
+
 if __name__ == "__main__":
     while True:
         print("1.Transforma o temperatura data dintr-o scara data intr-o alta scara data. ")
         print("2.Calculează CMMMC al n numere date.")
+        print("3.Determina ultimul numar prim mai mic decat un numar dat: ")
         print("X.Iesire")
         optiune=input("Dati optiunea: ")
         if optiune=="1":
@@ -77,8 +117,13 @@ if __name__ == "__main__":
                     number=int(input("Dati element: "))
                     numbers.append(number)
                 print(get_cmmmc(numbers))
+        elif optiune=="3":
+            nr=int(input("Dati numarul: "))
+            prime=get_largest_prime_below(nr)
+            print(f'ultimul numar prim mai mic decat {nr} este {prime}')
         elif optiune=="X":
             break
         else:
             print("Optiune gresita")
 
+    main()
